f10294e9f766a1e58a5ca88da2b0c1f9d1f18509
= <=>
2021-12-09 17:15:26 -0500

    started 9

---
+++ problems/09_problem.py
@@ -0,0 +1,52 @@
+"""
+Problem 9: Smoke Basin
+
+Premise - find local mins in a grid to avoid during navigation
+"""
+
+import numpy as np
+from copy import deepcopy
+
+# raw_data = open('09_test.txt', 'r').readlines()
+raw_data = open('09_input.txt', 'r').readlines()
+
+split_to_list = [list(l.strip()) for l in raw_data]
+ocean_floor = np.array(split_to_list).astype(int)
+
+# Part 1: Find local mins (value is lower than all surrounding cells)
+def pad(grid, dimensions=2, pad_value=0):
+    gs = grid.shape
+    g0 = [d + 2 for d in gs]
+    grid_pad = np.zeros(g0)
+    slices = [slice(1, d+1) for d in gs]
+    grid_pad[tuple(slices)] += grid
+    if pad_value != 0:
+        grid_pad[0, :] = pad_value
+        grid_pad[(gs[0] + 1), :] = pad_value
+        grid_pad[:, 0] = pad_value
+        grid_pad[:, (gs[1] + 1)]= pad_value
+    return grid_pad
+
+floor_padded = pad(deepcopy(ocean_floor), pad_value=9)
+
+# Start less elegantly
+min_locations = []
+smoke_values = []
+n_row, n_col = floor_padded.shape
+for i in range(1, n_row):
+    for j in range(1, n_col):
+        coord = [i, j]
+        center_value = floor_padded[i, j]
+        adjacent_cells = floor_padded[(i-1):(i+2), (j-1):(j+2)]
+        min_val = adjacent_cells.min()
+        if min_val == center_value:
+            min_locations.append(coord)
+            smoke_values.append(1 + center_value)
+
+print(f"Part 1: Total smoke risk is {sum(smoke_values)}")
+
+# Part 2: Find "basins", which are basically following the low point outwards until you hit 9s up, down, left, or right.
+# Find the three largest basins and multiply their values for the solution
+
+
+
