0c1a2117cf31b872a19c06cc56f8aeb4577b8228
Taryn Price <taryn.price@gmail.com>
2021-12-15 20:19:58 -0500

    Day 14 done

--- problems/14_problem.py
+++ problems/14_problem.py
@@ -1,47 +1,67 @@
 """
 Problem 14: Polymerization - string insertion
+
+Note: The question asks for total number of pairs in a long string (how many "CH"s and so on) - order does not matter.
 """
 
-raw_data = open('14_test.txt', 'r').readlines()
+raw_data = open('14_input.txt', 'r').readlines()
 start = raw_data[0].strip()
-rules = [] # Insertion process is ordered = no dict
+rules = {}
 for line in raw_data[2:]:
     pair, arrow, insert_element = line.strip().split()
-    rules.append((pair, insert_element))
+    rules[pair] = insert_element
 
 
-def insert_element_in_pair(rule_tuple):
-    pair, insert_element = rule_tuple
-    return ''.join([pair[0], insert_element, pair[1]])
+def split_pair(pair):
+    insert_letter = rules[pair]
+    new_pair1 = pair[0] + insert_letter
+    new_pair2 = insert_letter + pair[1]
+    return new_pair1, new_pair2
 
 
-def insert_in_polymer(polymer, rule_tuple, forbidden_indices):
-    replacement = insert_element_in_pair(rule_tuple)
-    first_indices_of_possible_pairs = [i for i in range(0, len(polymer)) if polymer[i:(i+2)] == rule_tuple[0]]
-    insert_indices = [fi + ind + 1 for ind, fi in enumerate(first_indices_of_possible_pairs)] # This is only if you insert all but you won't. fix
-    updated_list = []
-    for i, v in enumerated():
+def do_steps(steps):
+    initial_pairs = [start[i:(i + 2)] for i in range(len(start) - 1)]
+    pairs_counts = dict([(pair, start.count(pair)) for pair in initial_pairs])
 
-    updated_polymer = [polymer.replace(rule_tuple[0], replacement)] # update this line to avoid forbidden
-    return (insert_indices, updated_polymer)
+    for step in range(steps):
+        new_pairs = {}
+        for p, ct in pairs_counts.items():
+            split_pairs = split_pair(p)
+            for np in split_pairs:
+                if np in new_pairs:
+                    new_pairs[np] += 1 * ct
+                else:
+                    new_pairs[np] = ct
 
+        pairs_counts = new_pairs.copy()
 
-# Next step - update so in each step, no inserted elements are considered for any other rules
-def perform_insertions(polymer, rule_set, n_steps):
-    p = polymer
-    for s in range(n_steps):
-        indices_of_inserted_letters = []
-        for r in rule_set:
-            ind, p = insert_in_polymer(p, r)
-            [indices_of_inserted_letters.append(i) for i in ind]
-    return p
+    # Every letter is double-counted in dict except first and last:
+    letter_set = set(''.join(pairs_counts.keys()))
+    start_letter = start[0]
+    end_letter = start[1]
+    pairs_tuples = []
+    for l in letter_set:
+        l_count = 0
+        for pair, ct in pairs_counts.items():
+            if l in pair:
+                l_count += ct
+                if pair[0] == pair[1]:
+                    l_count += ct # Accounts for double letters like BB
+        if l in (start_letter, end_letter):
+            if start_letter == end_letter:
+                l_real = (l_count - 2) / 2 + 2
+            else:
+                l_real = (l_count - 1) / 2 + 1
+        else:
+            l_real = l_count / 2
+        pairs_tuples.append((l, l_real))
 
+    pairs_tuples.sort(key=lambda x: x[1])
+    max_count = pairs_tuples[-1][1]
+    min_count = pairs_tuples[0][1]
+    return max_count - min_count
 
-p1_polymer = perform_insertions(start, rules, 1)
-print(p1_polymer)
+print(f"Part 1: 10 steps = {do_steps(10)}") # 2027
+print(f"Part 2: 40 steps = {do_steps(40)}") # 2265039461737
 
-counts = [p1_polymer.count(l) for l in set(p1_polymer)]
-counts.sort()
-
-print(f"Part 1: max occur - min occur = {counts[-1] - counts[0]}")
 
