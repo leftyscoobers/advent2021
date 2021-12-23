aad9387e0447e2f436f62774b5392ab933c8b8a2
= <=>
2021-12-23 16:22:36 -0500

    fixed p15 part 1

--- problems/15_problem.py
+++ problems/15_problem.py
@@ -9,38 +9,49 @@ https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
 
 import numpy as np
 
+# Modify grid as we go out one coord at a time, updating 'least risky path".
+# Note that we don't actually need to record the shortest path - just the risk value
+
 raw_risk = [list(l.strip()) for l in open('15_input.txt', 'r').readlines()]
 risk_map = np.array(raw_risk).astype(int)
-dx, dy = risk_map.shape
-end = ((dx - 1), (dy - 1))
 
 
-# Modify grid as we go out one coord at a time, updating 'least risky path".
-# Note that we don't actually need to record the shortest path
-visited = set()
-risk_total = np.zeros((dx, dy)) + 1e10  # set to some initially huge number
-risk_total[(0, 0)] = 0  # start point risk is 0
+def find_least_risky_path(risk_map):
+    dx, dy = risk_map.shape
+    start = (0, 0)
+    end = ((dx - 1), (dy - 1))
 
-continue_from = {(0, 0)}
-while len(continue_from) > 0:
-    new_continue = set()
-    for coord in continue_from:
-        r, c = coord
-        visited.add(coord)
-        for n_r, n_c in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-            if (n_r, n_c) in visited:
-                continue
-            if not (0 <= n_r < dx and 0 <= n_c < dy):
-                continue
+    visited = set()
+    risk_total = np.zeros((dx, dy)) + 1e10  # set to some initially huge number
+    risk_total[end] = 0  # start point risk is 0, but we work backwards from the intended end
 
-            risk_at_neighbor = risk_total[(n_r, n_c)]
-            new_risk = risk_total[coord] + risk_map[(n_r, n_c)]
-            if new_risk < risk_at_neighbor:
-                risk_total[(n_r, n_c)] = new_risk
+    continue_from = {end}
+    while len(continue_from) > 0:
+        new_continue = set()
+        for coord in continue_from:
+            r, c = coord
+            visited.add(coord)
+            for n_r, n_c in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+                if (n_r, n_c) in visited:
+                    continue
+                if not (0 <= n_r < dx and 0 <= n_c < dy):
+                    continue
 
-            new_continue.add((n_r, n_c))
-    continue_from = new_continue
-    print(f"Length of continue from: {len(continue_from)}")
-    print(f"visited so far: {len(visited)}")
+                risk_at_neighbor = risk_total[(n_r, n_c)]
+                new_risk = risk_total[coord] + risk_map[(n_r, n_c)]
+                if new_risk < risk_at_neighbor:
+                    risk_total[(n_r, n_c)] = new_risk
 
-print(f"Risk at end point: {risk_total[end]}")  # 508 (currently outputs 510)
+                new_continue.add((n_r, n_c))
+        continue_from = new_continue
+
+    return risk_total[start]
+
+
+print(f"Part 1: Lowest risk = {find_least_risky_path(risk_map)}")  # Calculate this backwards from the end to the start
+
+
+# Part 2: Now make 5 x 5 tile of the above but every translation right or down increases risk by 1
+# (anything over 9 starts over at 1)
+
+
