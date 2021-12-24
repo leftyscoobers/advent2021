4b4b0cc50ec163b9bb82d3229050248b1548e371
Taryn Price <taryn.price@gmail.com>
2021-12-24 14:20:47 -0500

    completed p 15

--- problems/15_problem.py
+++ problems/15_problem.py
@@ -23,35 +23,54 @@ def find_least_risky_path(risk_map):
 
     visited = set()
     risk_total = np.zeros((dx, dy)) + 1e10  # set to some initially huge number
-    risk_total[end] = 0  # start point risk is 0, but we work backwards from the intended end
+    risk_total[start] = 0
 
-    continue_from = {end}
-    while len(continue_from) > 0:
-        new_continue = set()
-        for coord in continue_from:
-            r, c = coord
-            visited.add(coord)
-            for n_r, n_c in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
-                if (n_r, n_c) in visited:
-                    continue
-                if not (0 <= n_r < dx and 0 <= n_c < dy):
-                    continue
+    node_options = {start: 0}
+    cheapest = (0, start)
+    while node_options:
+        risk_so_far, coord = cheapest
+        if coord == end:
+            break
+        r, c = coord
+        visited.add(coord)
+        del node_options[coord]
+        for n_r, n_c in [(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)]:
+            if (n_r, n_c) in visited:
+                continue
+            if not (0 <= n_r < dx and 0 <= n_c < dy):
+                continue
 
-                risk_at_neighbor = risk_total[(n_r, n_c)]
-                new_risk = risk_total[coord] + risk_map[(n_r, n_c)]
-                if new_risk < risk_at_neighbor:
-                    risk_total[(n_r, n_c)] = new_risk
+            new_risk = risk_so_far + risk_map[(n_r, n_c)]
+            if new_risk < risk_total[(n_r, n_c)]:
+                risk_total[(n_r, n_c)] = new_risk
 
-                new_continue.add((n_r, n_c))
-        continue_from = new_continue
+            node_options[(n_r, n_c)] = risk_total[(n_r, n_c)]
 
-    return risk_total[start]
+        # Sort so we try lowest risk first
+        min_risk = min(node_options.values())
+        cheapest = [(r, coord) for coord, r in node_options.items() if r == min_risk][0]
+
+    return risk_total[end]
 
 
-print(f"Part 1: Lowest risk = {find_least_risky_path(risk_map)}")  # Calculate this backwards from the end to the start
+print(f"Part 1: Lowest risk = {find_least_risky_path(risk_map)}")
 
 
 # Part 2: Now make 5 x 5 tile of the above but every translation right or down increases risk by 1
 # (anything over 9 starts over at 1)
 
+def make_square_grid(original_tile, dimensions):
+    new_horiz = original_tile.copy()
+    for d in range(1, dimensions):
+        new_horiz = np.concatenate((new_horiz, original_tile + d), axis=1)
 
+    new_tile = new_horiz.copy()
+    for d in range(1, dimensions):
+        new_tile = np.concatenate((new_tile, new_horiz + d), axis=0)
+
+    new_tile = new_tile % 9
+    new_tile[new_tile == 0] = 9
+    return new_tile
+
+
+print(f"Part 2: Lowest risk from new tile-grid = {find_least_risky_path(make_square_grid(risk_map, 5))}")
